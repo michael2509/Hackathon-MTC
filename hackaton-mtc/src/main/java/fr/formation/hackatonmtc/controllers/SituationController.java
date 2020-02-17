3bd6a9a3b6f5c9aac708e2c1b558a05e3bc4659b
toboutal <toboutal@gmail.com>
2020-02-17 14:04:11 +0100

    second commit

---
+++ hackaton-mtc/src/main/java/fr/formation/hackatonmtc/controllers/SituationController.java
@@ -0,0 +1,10 @@
+package fr.formation.hackatonmtc.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("situation")
+public class SituationController {
+    
+
+}
