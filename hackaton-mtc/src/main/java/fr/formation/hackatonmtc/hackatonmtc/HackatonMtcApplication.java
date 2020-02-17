87bc896688017e343a216556fb2af60a77ba2a7d
Catherine NOBYN <cnobyn@gmail.com>
2020-02-17 12:33:49 +0100

    First commit

---
+++ hackaton-mtc/src/main/java/fr/formation/hackatonmtc/hackatonmtc/HackatonMtcApplication.java
@@ -0,0 +1,13 @@
+package fr.formation.hackatonmtc.hackatonmtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonMtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonMtcApplication.class, args);
+	}
+
+}
---
+++ hackaton-mtc/src/test/java/fr/formation/hackatonmtc/hackatonmtc/HackatonMtcApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.formation.hackatonmtc.hackatonmtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonMtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
